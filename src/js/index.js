cdf065b1bfe49f6ff041d85ccb44b78d4388734d
João Saro <joaovlsaro@gmail.com>
2020-02-19 23:52:37 +0000

    save-commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,128 @@
-const nr = 1;
+const nav = document.querySelector('nav');
+const navLinks = nav.querySelectorAll('nav__link');
+const section = document.querySelectorAll('section');
 
-const test = (a, b) => a + b
+function main() {
+  // nav.style.opacity = "1";
 
-const result = test(nr, 2);
+  navLinks.forEach((section, index) => {
+    section.addEventListener('click', section =>)
+  })
 
-console.log(result);
+  for (var i = 0; i < anchorItems.length; i++) {
+    anchorItems[i].onclick = function() {
+
+
+       // 
+       for (var i = 0; i < anchorItems.length; i++) {
+          $(anchorItems[i]).removeClass('selected');
+       }
+
+       for (var i = 0; i < sections.length; i++) {
+          $(sections[i]).removeClass("is-selected");
+       }
+
+       var href = this.getAttribute('href');
+
+      $(href).addClass("is-selected");
+      $(this).addClass('selected');
+
+       if (href === '#hero') {
+          list.style.marginBottom = '50px';
+       } else if (href === '#projects') {
+          list.style.marginBottom = '100px';
+       } else if (href === '#contacts') {
+          list.style.marginBottom = '150px';
+       }
+    }
+  }
+
+}
+
+
+
+window.onload = function() {
+  main();
+}
+
+///
+
+function main() {
+    list.style.opacity = "1";
+    for (var i = 0; i < anchorItems.length; i++) {
+      anchorItems[i].onclick = function() {
+
+         for (var i = 0; i < anchorItems.length; i++) {
+            $(anchorItems[i]).removeClass('selected');
+         }
+
+         for (var i = 0; i < sections.length; i++) {
+            $(sections[i]).removeClass("is-selected");
+         }
+
+         var href = this.getAttribute('href');
+
+        $(href).addClass("is-selected");
+        $(this).addClass('selected');
+
+         if (href === '#hero') {
+            list.style.marginBottom = '50px';
+         } else if (href === '#projects') {
+            list.style.marginBottom = '100px';
+         } else if (href === '#contacts') {
+            list.style.marginBottom = '150px';
+         }
+      }
+    }
+
+}
+
+$('.what-trigger').on('click', function () {
+   for (var i = 0; i < anchorItems.length; i++) {
+      $(anchorItems[i]).removeClass('selected');
+   }
+
+   for (var i = 0; i < sections.length; i++) {
+      $(sections[i]).removeClass("is-selected");
+   }
+
+   var href = this.getAttribute('href');
+
+  $(href).addClass("is-selected");
+  $(anchorItems[1]).addClass('selected');
+
+  if (href === '#hero') {
+     list.style.marginBottom = '50px';
+  } else if (href === '#projects') {
+     list.style.marginBottom = '100px';
+  } else if (href === '#contacts') {
+     list.style.marginBottom = '150px';
+  }
+});
+
+$('.where-trigger').on('click', function () {
+   for (var i = 0; i < anchorItems.length; i++) {
+      $(anchorItems[i]).removeClass('selected');
+   }
+
+   for (var i = 0; i < sections.length; i++) {
+      $(sections[i]).removeClass("is-selected");
+   }
+
+   var href = this.getAttribute('href');
+
+  $(href).addClass("is-selected");
+  $(anchorItems[2]).addClass('selected');
+
+  if (href === '#hero') {
+     list.style.marginBottom = '50px';
+  } else if (href === '#projects') {
+     list.style.marginBottom = '100px';
+  } else if (href === '#contacts') {
+     list.style.marginBottom = '150px';
+  }
+});
+
+window.onload = function() {
+   main();
+}
