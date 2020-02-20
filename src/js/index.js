2b9f33d2bb8577e0fe52d2fd7f5a790d6c280df8
João Saro <joaovlsaro@gmail.com>
2020-02-20 00:22:49 +0000

    links with modifiers by script

--- src/js/index.js
+++ src/js/index.js
@@ -1,128 +1,91 @@
-const nav = document.querySelector('nav');
-const navLinks = nav.querySelectorAll('nav__link');
-const section = document.querySelectorAll('section');
+const nav = document.querySelector('.nav');
+const navSections = nav.querySelectorAll('.nav__link');
+const sections = document.querySelectorAll('.section');
 
-function main() {
-  // nav.style.opacity = "1";
+function jumpSection (event) {
+  const { target } = event;
+  const sectionLink = this.getAttribute('href');
+  const sectionToJump = document.querySelector(sectionLink);
 
-  navLinks.forEach((section, index) => {
-    section.addEventListener('click', section =>)
+  navSections.forEach(sectionLink => {
+    sectionLink.classList.remove('nav__links--active');
   })
 
-  for (var i = 0; i < anchorItems.length; i++) {
-    anchorItems[i].onclick = function() {
+  sections.forEach(section => {
+    section.classList.remove('section--selected');
+  })
 
+  target.classList.add('nav__links--active');
+  sectionToJump.classList.add('section--selected');
 
-       // 
-       for (var i = 0; i < anchorItems.length; i++) {
-          $(anchorItems[i]).removeClass('selected');
-       }
-
-       for (var i = 0; i < sections.length; i++) {
-          $(sections[i]).removeClass("is-selected");
-       }
-
-       var href = this.getAttribute('href');
-
-      $(href).addClass("is-selected");
-      $(this).addClass('selected');
-
-       if (href === '#hero') {
-          list.style.marginBottom = '50px';
-       } else if (href === '#projects') {
-          list.style.marginBottom = '100px';
-       } else if (href === '#contacts') {
-          list.style.marginBottom = '150px';
-       }
-    }
-  }
-
+  // if (href === '#hero') {
+  //     list.style.marginBottom = '50px';
+  // } else if (href === '#projects') {
+  //     list.style.marginBottom = '100px';
+  // } else if (href === '#contacts') {
+  //     list.style.marginBottom = '150px';
+  // }
 }
 
 
+function main() { 
+  navSections.forEach(navSection => {
+    navSection.addEventListener('click', jumpSection);
+  });
+}
 
+// Replace event listener
 window.onload = function() {
   main();
 }
 
-///
 
-function main() {
-    list.style.opacity = "1";
-    for (var i = 0; i < anchorItems.length; i++) {
-      anchorItems[i].onclick = function() {
+// $('.what-trigger').on('click', function () {
+//    for (var i = 0; i < anchorItems.length; i++) {
+//       $(anchorItems[i]).removeClass('selected');
+//    }
 
-         for (var i = 0; i < anchorItems.length; i++) {
-            $(anchorItems[i]).removeClass('selected');
-         }
+//    for (var i = 0; i < sections.length; i++) {
+//       $(sections[i]).removeClass("is-selected");
+//    }
 
-         for (var i = 0; i < sections.length; i++) {
-            $(sections[i]).removeClass("is-selected");
-         }
+//    var href = this.getAttribute('href');
 
-         var href = this.getAttribute('href');
+//   $(href).addClass("is-selected");
+//   $(anchorItems[1]).addClass('selected');
 
-        $(href).addClass("is-selected");
-        $(this).addClass('selected');
+//   if (href === '#hero') {
+//      list.style.marginBottom = '50px';
+//   } else if (href === '#projects') {
+//      list.style.marginBottom = '100px';
+//   } else if (href === '#contacts') {
+//      list.style.marginBottom = '150px';
+//   }
+// });
 
-         if (href === '#hero') {
-            list.style.marginBottom = '50px';
-         } else if (href === '#projects') {
-            list.style.marginBottom = '100px';
-         } else if (href === '#contacts') {
-            list.style.marginBottom = '150px';
-         }
-      }
-    }
+// $('.where-trigger').on('click', function () {
+//    for (var i = 0; i < anchorItems.length; i++) {
+//       $(anchorItems[i]).removeClass('selected');
+//    }
 
-}
+//    for (var i = 0; i < sections.length; i++) {
+//       $(sections[i]).removeClass("is-selected");
+//    }
 
-$('.what-trigger').on('click', function () {
-   for (var i = 0; i < anchorItems.length; i++) {
-      $(anchorItems[i]).removeClass('selected');
-   }
+//    var href = this.getAttribute('href');
 
-   for (var i = 0; i < sections.length; i++) {
-      $(sections[i]).removeClass("is-selected");
-   }
+//   $(href).addClass("is-selected");
+//   $(anchorItems[2]).addClass('selected');
 
-   var href = this.getAttribute('href');
+//   if (href === '#hero') {
+//      list.style.marginBottom = '50px';
+//   } else if (href === '#projects') {
+//      list.style.marginBottom = '100px';
+//   } else if (href === '#contacts') {
+//      list.style.marginBottom = '150px';
+//   }
+// });
 
-  $(href).addClass("is-selected");
-  $(anchorItems[1]).addClass('selected');
-
-  if (href === '#hero') {
-     list.style.marginBottom = '50px';
-  } else if (href === '#projects') {
-     list.style.marginBottom = '100px';
-  } else if (href === '#contacts') {
-     list.style.marginBottom = '150px';
-  }
-});
-
-$('.where-trigger').on('click', function () {
-   for (var i = 0; i < anchorItems.length; i++) {
-      $(anchorItems[i]).removeClass('selected');
-   }
-
-   for (var i = 0; i < sections.length; i++) {
-      $(sections[i]).removeClass("is-selected");
-   }
-
-   var href = this.getAttribute('href');
-
-  $(href).addClass("is-selected");
-  $(anchorItems[2]).addClass('selected');
-
-  if (href === '#hero') {
-     list.style.marginBottom = '50px';
-  } else if (href === '#projects') {
-     list.style.marginBottom = '100px';
-  } else if (href === '#contacts') {
-     list.style.marginBottom = '150px';
-  }
-});
-
-window.onload = function() {
-   main();
-}
+// window.onload = function() {
+//    main();
+// }
