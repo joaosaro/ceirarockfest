1a1bcf0a3779dd3b002522d57e3d8f311915ef26
João Saro <joaovlsaro@gmail.com>
2020-02-25 20:19:08 +0000

    Add section opacity swap

--- src/js/index.js
+++ src/js/index.js
@@ -9,21 +9,22 @@ function jumpSection (event) {
   const navigation = target.parentElement;
   const targetIndex = Array.from(navigation.children).indexOf(target);
 
+  // Remove selected section
   navSections.forEach(sectionLink => {
     sectionLink.classList.remove('nav__links--active');
   })
-
+  
   sections.forEach(section => {
     section.classList.remove('section--selected');
   })
-
+  
+  // Add new selected section
   target.classList.add('nav__links--active');
   sectionToJump.classList.add('section--selected');
 
-
+  // Animate nav links - by changing bottom padding
   navigation.style.setProperty('--nav-padding-bottom', targetIndex * 15);
 }
-
 
 function main() { 
   navSections.forEach(navSection => {
